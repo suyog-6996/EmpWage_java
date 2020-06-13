7efcde677d453a8148ff9a4064d021fb515c98e8
suyog-6996 <gabhane.suyog@gmail.com>
2020-06-13 12:54:03 +0530

    ADDED:Using Class Method

--- EmpWage.java
+++ EmpWage.java
@@ -1,1 +1,40 @@
+public class EmpWage {
 
+                public static final int IS_PART_TIME = 1;
+                public static final int IS_FULL_TIME = 2;
+                public static final int EMP_RATE_PER_HOUR = 20;
+                public static final int NUM_OF_WORKING_DAYS = 2;
+                public static final int MAX_HRS_IN_MONTH = 10;
+
+                public static int computeEmpWage()
+                {
+                        int empHrs = 0;
+                        int totalEmpHrs = 0;
+                        int totalWorkingDays = 0;
+                        while (totalEmpHrs <= MAX_HRS_IN_MONTH &&
+                                        totalWorkingDays < NUM_OF_WORKING_DAYS){
+                                totalWorkingDays++;
+                        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+                        switch (empCheck)
+                        {
+                                case IS_PART_TIME:
+                                        empHrs = 4;
+                                        break;
+                                case IS_FULL_TIME :
+                                        empHrs = 8;
+                                        break;
+                                        default:
+                                                empHrs = 0;
+                        }
+                        totalEmpHrs += empHrs;
+                        System.out.println("Day"+ totalWorkingDays + "Emp Hr: " +empHrs);
+                }
+                        int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+           System.out.println("Total Employee Wage:"+ totalEmpWage);
+           return totalEmpWage;
+        }
+public static void main(String[] args)
+ {
+        computeEmpWage();
+ }
+}
